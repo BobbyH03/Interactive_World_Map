1fc0526c84f10ef06ead3ff0a3c682235f404521
Robert Hoskins <137965495+BobbyH03@users.noreply.github.com>
2025-01-08 19:06:35 -0600

    Update app.component.ts
    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
@@ -9,10 +9,12 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Interactive World Map';
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title) {}
+
+  ngOnInit() {
     this.titleService.setTitle(this.title);
   }
 }
