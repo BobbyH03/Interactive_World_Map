0f48a1126278fee0ce91fdb447dd4e1e85df3805
Robert Hoskins <137965495+BobbyH03@users.noreply.github.com>
2025-01-08 19:01:59 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Interactive World Map';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title);  // Set the page's title
+  }
 }
