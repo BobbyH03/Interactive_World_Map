38c8004fdee0d322797f6ca28a1efc5a7cd2b6a9
Robert Hoskins <137965495+BobbyH03@users.noreply.github.com>
2025-01-08 18:53:48 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { WorldmapComponent } from './worldmap/worldmap.component';
 
 export const routes: Routes = [
-    {path:'worldmap', component: WorldmapComponent},
+    {path:'worldmap', component: WorldmapComponent, title: 'Interactive Map'},
     { path: '', redirectTo: '/worldmap', pathMatch:'full'}
 ];
